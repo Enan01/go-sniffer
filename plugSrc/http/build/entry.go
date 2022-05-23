1df132b4063678fb0a62c992dc3a57e9823ce992
Enan01 <enanzhou@foxmail.com>
2022-05-23 15:04:52 +0800

    feat: print http header & support print http response

--- plugSrc/http/build/entry.go
+++ plugSrc/http/build/entry.go
@@ -14,17 +14,17 @@ import (
 )
 
 const (
-	Port       = 80
-	Version    = "0.1"
+	Port    = 80
+	Version = "0.1"
 )
 
 const (
-	CmdPort    = "-p"
+	CmdPort = "-p"
 )
 
 type H struct {
-	port       int
-	version    string
+	port    int
+	version string
 }
 
 var hp *H
@@ -32,73 +32,110 @@ var hp *H
 func NewInstance() *H {
 	if hp == nil {
 		hp = &H{
-			port   :Port,
-			version:Version,
+			port:    Port,
+			version: Version,
 		}
 	}
 	return hp
 }
 
 func (m *H) ResolveStream(net, transport gopacket.Flow, buf io.Reader) {
-
 	bio := bufio.NewReader(buf)
 	for {
-		req, err := http.ReadRequest(bio)
-
-		if err == io.EOF {
-			return
-		} else if err != nil {
-			continue
+		// log.Println(net, transport)
+		if strconv.Itoa(m.port) == transport.Dst().String() {
+			decodeRequest(bio)
 		} else {
-
-			var msg = "["
-			msg += req.Method
-			msg += "] ["
-			msg += req.Host + req.URL.String()
-			msg += "] ["
-			body, err := ioutil.ReadAll(req.Body)
-			if err != nil {
-				log.Println("read request body err", err)
-				continue
-			}
-			// req.ParseForm()
-			// msg += req.Form.Encode()
-			msg += string(body)
-			msg += "]"
-
-			log.Println(msg)
-
-			req.Body.Close()
+			decodeResponse(bio)
 		}
 	}
 }
 
+func decodeRequest(bio *bufio.Reader) {
+	req, err := http.ReadRequest(bio)
+	if err == io.EOF {
+		log.Println("decode request err", err)
+		return
+	} else if err != nil {
+		log.Println("decode request err", err)
+		return
+	} else {
+		var msg = "request ["
+		msg += req.Method
+		msg += "] ["
+		msg += req.Host + req.URL.String()
+		msg += "] ["
+		msg += fmt.Sprintf("%+v", req.Header)
+		msg += "] ["
+
+		body, err := ioutil.ReadAll(req.Body)
+		if err != nil {
+			log.Println("read request body err", err)
+			return
+		}
+		// req.ParseForm()
+		// msg += req.Form.Encode()
+		msg += string(body)
+		msg += "]"
+
+		log.Println(msg)
+
+		req.Body.Close()
+	}
+}
+
+func decodeResponse(bio *bufio.Reader) {
+	rsp, err := http.ReadResponse(bio, nil)
+	if err == io.EOF {
+		// log.Println("decode response err", err)
+		return
+	} else if err != nil {
+		// log.Println("decode response err", err)
+		return
+	} else {
+		var msg = "response ["
+		msg += fmt.Sprintf("%+v", rsp.Header)
+		msg += "] ["
+		body, err := ioutil.ReadAll(rsp.Body)
+		if err != nil {
+			// log.Println("read response body err", err)
+			return
+		}
+		msg += string(body)
+		msg += "]"
+
+		// log.Println(msg)
+
+		rsp.Body.Close()
+	}
+}
+
 func (m *H) BPFFilter() string {
-	return "tcp and port "+strconv.Itoa(m.port);
+	return "tcp and port " + strconv.Itoa(m.port)
 }
 
 func (m *H) Version() string {
 	return Version
 }
 
-func (m *H) SetFlag(flg []string)  {
+func (m *H) SetFlag(flg []string) {
 
 	c := len(flg)
 
 	if c == 0 {
 		return
 	}
-	if c >> 1 == 0 {
+	if c>>1 == 0 {
 		fmt.Println("ERR : Http Number of parameters")
 		os.Exit(1)
 	}
-	for i:=0;i<c;i=i+2 {
+	for i := 0; i < c; i = i + 2 {
 		key := flg[i]
 		val := flg[i+1]
 
 		switch key {
 		case CmdPort:
-			port, err := strconv.Atoi(val);
+			port, err := strconv.Atoi(val)
 			m.port = port
 			if err != nil {
 				panic("ERR : port")
